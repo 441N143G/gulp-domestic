c96d34c91dab41713b8a7c589fb6c0816671f436
edish edish <edish edish>
2016-08-04 18:53:56 +0300

    lvlMaker

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,56 @@
 'use strict'
-let gulp = require('gulp');
-let rename = require("gulp-rename");
-
-let config = {
-    dirName: 'Машина_Времени',
-    extension: 'mp3'
-}
+let gulp = require('gulp'),
+    rename = require("gulp-rename"),
+    getPixels = require("get-pixels"),
+    RenameConfig = {
+        dirName: 'Uriah Heep',
+        extension: 'mp3'
+    },
+    lvlConfig = {
+        name: 'lvl1',
+        fileExt: 'txt'
+    }
 gulp.task('rename', function() {
-    return gulp.src(`./${config.dirName}/**/*.${config.extension}`)
+    return gulp.src(`./${RenameConfig.dirName}/**/*.${  RenameConfig.extension}`)
         .pipe(rename(function(path) {
             let name = path.basename;
-            path.basename  = (name.split(/-|\./)[1] || path.basename).trim()
+            path.basename = (name.split(/-|\./)[1] || path.basename).trim()
         }))
-        .pipe(gulp.dest(`./dist/${config.dirName}/`));
+        .pipe(gulp.dest(`./dist/${RenameConfig.dirName}/`));
+});
+gulp.task('pixels', function() {
+    let res = []
+    let writer = `export let mapModel = [
+    `
+    return getPixels("test.png", function(err, pixels) {
+        if (err) {
+            console.log("Bad image path")
+            return
+        }
+        for (let i = 0; i < pixels.shape[1]; i++) {
+            res.push([])
+            for (let j = 0; j < pixels.shape[0]; j++) {
+                let pusher = pixels.get(j, i, 0) === 255 ? 101 : 201
+                res[i].push([pusher])
+            }
+        }
+        for (let it of res) {
+            writer += `[`
+            for (let i of it) {
+
+                writer += `[${i}]`
+                if (it.indexOf(i) < (it.length - 1)) {
+                    writer += `,`
+                }
+            }
+            writer += `]`
+            if (res.indexOf(it) < (res.length - 1)) {
+                writer += `,
+            `
+            }
+        }
+        writer += `
+      ]`
+        require('fs').writeFileSync(`dist/${lvlConfig.name}.${lvlConfig.fileExt}`, `${writer}`);
+    })
 });
